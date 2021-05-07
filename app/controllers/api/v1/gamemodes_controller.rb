885bba7a8113e17bd9e55dcdaf5d119b5b740704
github username <github email address>
2021-05-07 11:04:52 -0500

    authen init

--- app/controllers/api/v1/gamemodes_controller.rb
+++ app/controllers/api/v1/gamemodes_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::GamemodesController < ApplicationController
+
+    before_action :logged_in?
+
+    def index 
+        gamemodes = Gamemodes.all
+        render json: gamemodes
+    end
+
 end
