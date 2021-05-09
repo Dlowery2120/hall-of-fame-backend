d7cf4f82486fc159937b50df2de4d2713344f296
Devante Lowery <dlowery2120@gmail.com>
2021-05-09 16:47:05 -0500

    added index and show to controllers

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::QuestionsController < ApplicationController
+    def index 
+        questions = Question.all
+        render json: questions, except: [:created_at, :updated_at]
+    end
+
+    def show
+        question = Question.find_by(id: params[:id])
+        if question
+          render json: question, except: [:created_at, :updated_at]
+        else
+          render json: {message: 'Question not found'}
+        end
+    end
 end
--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -5,5 +5,14 @@ class Api::V1::SongsController < ApplicationController
         render json: songs
     end
 
+    def show
+        song = Song.find_by(id: params[:id])
+        if song
+          render json: song, except: [:created_at, :updated_at]
+        else
+          render json: {message: 'Song not found'}
+        end
+    end
+
 
 end
